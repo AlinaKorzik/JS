ca15180cef943bee4f3f00bd92e3ba1d1b2f4ca9
AlinaKorzik <alina.korzik95@mail.ru>
2022-02-13 14:12:52 +0300

    task16_change

--- task16/script.js
+++ task16/script.js
@@ -1,49 +1,38 @@
+function cleanRoom(dirtyLevel) { 
+
+    return new Promise((resolve, reject) => {
+
+        if(dirtyLevel <=0) {
+
+            reject('Все чисто, уборка не требуется!')
+        } else if (dirtyLevel > 10) {
+
+            reject('Комната слишком грязная! Вызывайте клининг!')
+        } else {
+
+            setTimeout(()=> resolve(`Комната убрана за ${dirtyLevel} секунд.`), dirtyLevel * 1000)
+        }
+    })
+ }
+
 async function cleanRoomsStepByStep(dirtyLevel1, dirtyLevel2, dirtyLevel3) {
+    
+    try {
 
-    let promise_1 = new Promise (function (resolve, reject) {
+        const room1 = await cleanRoom(dirtyLevel1)
 
-        let cleaningTime = dirtyLevel1 * 1000
-    
-        if (dirtyLevel1 > 0 && dirtyLevel1 <= 10) {
-    
-            setTimeout(() => resolve(`Уборка первой комнаты завершена`), cleaningTime)
-        }
-    });
-    
-    let promise_2 = new Promise (function (resolve, reject) {
-    
-        cleaningTime = dirtyLevel2 * 1000
-    
-        if(dirtyLevel2 > 0 && dirtyLevel2 <= 10) {
-    
-            setTimeout(() => resolve(`Уборка второй комнаты завершена`), cleaningTime)
-        }
-    });
-    
-    let promise_3 = new Promise (function (resolve, reject) {
-    
-        cleaningTime = dirtyLevel3 * 1000
-    
-            if(dirtyLevel3 > 0 && dirtyLevel3 <= 10) {
-    
-                setTimeout(() => resolve(`Уборка третьей комнаты завершена`), cleaningTime)
-            }               
-    });
+        const room2 = await cleanRoom(dirtyLevel2)
 
-    if(dirtyLevel1 < 0 || dirtyLevel1 > 10 || dirtyLevel2 < 0 || dirtyLevel2 > 10 || dirtyLevel3 < 0 || dirtyLevel3 > 10) {
+        const room3 = await cleanRoom(dirtyLevel3)
 
-        console.error ('Превышен уровень загрязнения! Уборка не проводится!')
-    } else {
+        console.log(room1, room2, room3)
+    } 
+    
+    catch(err) {
 
-        const room1 = await promise_1
-        console.log(room1)
-
-        const room2 = await promise_2
-        console.log(room2)
-
-        const room3 = await promise_3
-        console.log(room3)
+        console.error(err)
     }
+    
 }
 
 cleanRoomsStepByStep(3,5,4)
--- task16/script1.js
+++ task16/script1.js
@@ -1,21 +1,26 @@
+function cleanRoom(dirtyLevel) { 
+
+    return new Promise((resolve, reject) => {
+
+        if(dirtyLevel <=0) {
+
+            reject('Все чисто, уборка не требуется!')
+        } else if (dirtyLevel > 10) {
+
+            reject('Комната слишком грязная! Вызывайте клининг!')
+        } else {
+
+            setTimeout(()=> resolve(`Комната убрана за ${dirtyLevel} секунд.`), dirtyLevel * 1000)
+        }
+    })
+ }
+
 async function cleanRoomsStepByStep(dirtyLevel1, dirtyLevel2, dirtyLevel3) {
 
     try {
 
-        let promise_1 = new Promise (function (resolve, reject) {
+        const room1 = await cleanRoom(dirtyLevel1)
 
-            let cleaningTime = dirtyLevel1 * 1000
-    
-            if (dirtyLevel1 > 0 && dirtyLevel1 <= 10) {
-    
-                setTimeout(() => resolve(`Уборка первой комнаты завершена`), cleaningTime)
-            } else {
-    
-                reject(new Error('Уборка первой комнаты невозможна, проверяем остальные!'))
-            }
-        });
-
-        const room1 = await promise_1
         console.log(room1)
 
     } catch (err) {
@@ -25,20 +30,8 @@ async function cleanRoomsStepByStep(dirtyLevel1, dirtyLevel2, dirtyLevel3) {
 
     try {
     
-        let promise_2 = new Promise (function (resolve, reject) {
-    
-            cleaningTime = dirtyLevel2 * 1000
-    
-            if(dirtyLevel2 > 0 && dirtyLevel2 <= 10) {
-    
-                setTimeout(() => resolve(`Уборка второй комнаты завершена`), cleaningTime)
-            } else{
-                       
-                reject(new Error('Уборка второй комнаты невозможна, проверяем остальные!'))
-            }
-        });
+        const room2 = await cleanRoom(dirtyLevel2)
 
-        const room2 = await promise_2
         console.log(room2)
     
     } catch(err) {
@@ -48,26 +41,14 @@ async function cleanRoomsStepByStep(dirtyLevel1, dirtyLevel2, dirtyLevel3) {
 
     try {
 
-        let promise_3 = new Promise (function (resolve, reject) {
-    
-            cleaningTime = dirtyLevel3 * 1000
-        
-                if(dirtyLevel3 > 0 && dirtyLevel3 <= 10) {
-        
-                    setTimeout(() => resolve(`Уборка третьей комнаты завершена`), cleaningTime)
-                } else {
-        
-                    reject(new Error('Уборка третьей комнаты невозможна!'))
-                }                  
-        });
+        const room3 = await cleanRoom(dirtyLevel3)
 
-        const room3 = await promise_3
         console.log(room3)
 
     } catch(err) {
 
         console.error(err)
     } 
-    }
+}
 
-cleanRoomsStepByStep(3,3,12)
+cleanRoomsStepByStep(3,12,2)
